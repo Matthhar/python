cff22045413f960de0e141d3d9efe50849c62c85
(s) Harry Matthews <htmatthews@pm7-0272.uopnet.plymouth.ac.uk>
2016-10-25 17:11:38 +0100

    Ball moving smoothly

--- Graphics/visualization.py
+++ Graphics/visualization.py
@@ -6,13 +6,7 @@ data = [52, 47, 57, 49, 59, 62, 44, 76, 52, 52, 44, 59, 59, 79, 59, 42, 57, 48, 
 
 window = GraphWin("Visualisation",500,500)
 
-def myobject():
-    ball = Circle(Point(y,x),50)
-    ball.setFill(color_rgb(r,g,b))
-    ball.draw(window)
-    sleep(0.2)
-    ball.undraw()
-    
+
 y = 100
 x = 200
 yspeed = 10
@@ -21,16 +15,21 @@ r = 40
 g = 200
 b = 120
 colourchange = 1
-
-
+ball = Circle(Point(y,x),50)
+ball.setFill(color_rgb(r,g,b))
+ball.setOutline(color_rgb(r,g,b))
+ball.draw(window)
 
 
 while True:
     
+       
+    ball.move(yspeed,xspeed)
+    ball.setFill(color_rgb(r,g,b))
+    ball.setOutline(color_rgb(r,g,b))
+    sleep(0.2)
     y = y + yspeed
     x = x + xspeed
-    myobject()
-    
     
     if y == 450:
         yspeed = -10
