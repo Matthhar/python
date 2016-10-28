8cbd19557b06be5d73437ed61004cc5d86b1df68
(s) Harry Matthews <htmatthews@pm7-0070.uopnet.plymouth.ac.uk>
2016-10-28 14:42:24 +0100

    working on history

--- Twitter/Twitterread.py
+++ Twitter/Twitterread.py
@@ -1,12 +1,17 @@
 import twitter
-
-user = 15439395
+import datetime
 
 file = open("/Users/htmatthews/Documents/python/Twitter/Twittercredentials.txt")
 creds = file.read().split('\n')
 
 api = twitter.Api(creds[0],creds[1],creds[2],creds[3])
 
-statuses = api.GetUserTimeline(user)
+file = open("/Users/htmatthews/Library/Application Support/Google/Chrome/Default/History")
+sites = file.read().split('\n')
 
-print (statuses[0].text)
+
+print sites[len(sites)-1]
+#timestamp = datetime.datetime.utcnow()
+
+#response = api.PostUpdate("Tweeted at " + str(timestamp))
+#print ("Status updated to: " + response.text)
